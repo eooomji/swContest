55a75f6b52136127ebcc89001061b96b7e68d488
eooomji <70639677+eooomji@users.noreply.github.com>
2021-10-06 00:10:53 +0900

    2021.10.06.수 12:10

--- WebRTC_part/src/server.js
+++ WebRTC_part/src/server.js
@@ -1,21 +1,21 @@
 import http from "http";
 import SocketIO from "socket.io";
-import express from "express";
+import express from "express";    
 
 const app = express();
 
-app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
-app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (_, res) => res.render("home"));
+app.set("view engine", "pug"); // Pug로 view engine 설정
+app.set("views", __dirname + "/views"); // Express에 template이 어딨는지 지정 
+app.use("/public", express.static(__dirname + "/public")); // public url 생성 -> user에게 파일 공유
+app.get("/", (_, res) => res.render("home")); // home.pug를 render 해주는 route handler 만듬
 app.get("/*", (_, res) => res.redirect("/"));
 
-const httpServer = http.createServer(app);
+const httpServer = http.createServer(app); // webSocket을 하려면 꼭 필요한 부분임 => server가 만들어짐. 경로는 app.js
 const wsServer = SocketIO(httpServer);
 
+// socket에는 이미 room 기능이 내장되어 있음
 wsServer.on("connection", (socket) => {
-    socket.on("join_room", (roomName) => {
-        socket.join(roomName);
+    socket.on("join_room", (roomName) => {  // join_room : event 
         socket.to(roomName).emit("welcome");
 });
     socket.on("offer", (offer, roomName) => {
@@ -32,10 +32,8 @@ wsServer.on("connection", (socket) => {
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
 
-// import http from "http";
 // import {Server} from "socket.io";
 // import { instrument } from "@socket.io/admin-ui";
-// import express from "express";
 
 // const app = express();
 
@@ -105,26 +103,31 @@ httpServer.listen(3000, handleListen);
 //     socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
 // });
 
-// // const server = http.createServer(app);
-// // const wss = new WebSocket.Server({ server });
-// // const sockets = [];
-// // wss.on("connection", (socket) => {
-// //     sockets.push(socket);
-// //     socket["nickname"] = "Anon";
-// //     console.log("Connected to Browser");
-// //     socket.on("close", onSocketClose);
-// //     socket.on("message", (roomName) => { 
-// //         const message = JSON.parse(roomName);
-// //         switch (message.type){
-// //             case "new_message":
-// //                 sockets.forEach((aSocket) =>
-// //                  aSocket.send(`${socket.nickname}: ${message.payload}`)
-// //                 );
-// //             case "nickname":
-// //                 socket["nickname"] = message.payload;
-// //         }
-// //     });
-// // });
+// ========= http 서버, webSocket 서버 둘 다 만듬. 꼭 안 그래도 됨
+// http protocol과 ws connection 지원함
+// http를 만든 이유는, views, statifc files, home, redirection을 원하기 때문
+// const server = http.createServer(app);
+// const wss = new WebSocket.Server({ server }); // wss : WebSocket Server - 서버를 전달해줄 수 있음. http 서버 위에 wetSocket 서버 만듬
+
+// const sockets = [];
+
+// wss.on("connection", (socket) => { // connection이 생기면서 socket을 받음
+//     sockets.push(socket);
+//     socket["nickname"] = "Anon"; // socket에 nickname을 줌
+//     console.log("Connected to Browser");
+//     socket.on("close", onSocketClose);   // close를 listen 해줌
+//     socket.on("message", (roomName) => { // socket이 message를 보낼때까지 기다림
+//         const message = JSON.parse(roomName);
+//         switch (message.type){   // message.type에 따른 switch문
+//             case "new_message":  // message type이 new_message인 경우
+//                 sockets.forEach((aSocket) =>
+//                  aSocket.send(`${socket.nickname}: ${message.payload}`)  // nickname property를 socket object에 저장
+//                 );
+//             case "nickname":
+//                 socket["nickname"] = message.payload;    // 사용자로부터 받은 nickname을 socket에 넣어줌 => socket 안에 정보 저장 가능!
+//         }
+//     });
+// });
 
 // const handleListen = () => console.log('Listening on http://localhost:3000');
 // httpServer.listen(3000, handleListen);
