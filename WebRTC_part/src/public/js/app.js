444ce13dc556e35c9c31ee3dc11bdd2a074e4fdb
eooomji <70639677+eooomji@users.noreply.github.com>
2021-10-06 14:28:59 +0900

    Update app.js

--- WebRTC_part/src/public/js/app.js
+++ WebRTC_part/src/public/js/app.js
@@ -1,3 +1,4 @@
+// Frontend
 const socket = io();    // socket_io를 써주기 위함
 
 const myFace = document.getElementById("myFace");
@@ -67,6 +68,7 @@ function handleMuteClick() {
     muted = false;
   }
 }
+
 function handleCameraClick() {
   myStream
     .getVideoTracks()
@@ -121,7 +123,7 @@ async function handleWelcomeSubmit(event) {
 welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 
 // Socket Code
-
+// server.js 21번째 코드와 연결
 socket.on("welcome", async () => {
     const offer = await myPeerConnection.createOffer();
     myPeerConnection.setLocalDescription(offer);
@@ -168,95 +170,7 @@ function handleAddstream(data){
     const peerFace = document.getElementById("peerFace");
     peerFace.srcObject = data.stream;
 }
-// const socket = io();
 
-// const myFace = document.getElementById("myFace");
-// const muteBtn = document.getElementById("mute");
-// const cameraBtn = document.getElementById("camera");
-// const camerasSelect = document.getElementById("cameras");
-// const call = document.getElementById("call");
-
-// call.hidden = true;
-
-// let myStream;
-// let muted = false;
-// let cameraOff = false;
-// let roomName;
-// let myPeerConnection;
-
-// async function getCameras(){
-//     try {
-//         const devices = await navigator.mediaDevices.enumerateDevices();
-//         const cameras = devices.filter(device => device.kind === "videoinput");
-//         const currentCamera = myStream.getVideoTracks() [0];
-//         cameras.forEach(camera => {
-//             const option = document.createElement("option");
-//             option.value = camera.deviceId;
-//             option.innerText = camera.label;
-//             if(currentCamera.label === camera.label){
-//                 option.selected = true;
-//             }
-//             camerasSelect.appendChild(option);
-//         });
-//     }catch(e){
-//         console.log(e);
-//     }
-// }
-
-// async function getMedia(deviceId){
-//     const initialConstrains = {
-//         audio: true, 
-//         video: { facingMode: "user" },
-//     };
-//     const cameraConstraints = {
-//         audio: true,
-//         video: {deviceId : { exact: deviceId } },
-//     };
-//     try {
-//         myStream = await navigator.mediaDevices.getUserMedia(
-//             deviceId? cameraConstraints : initialConstrains
-//         );
-//         myFace.srcObject = myStream;
-//         if(!deviceId){
-//             await getCameras();
-//         }
-//     } catch(e) {
-//         console.log(e);
-//     }
-// }
-
-// function handleMuteClick () {
-//     myStream
-//         .getAudioTracks()
-//         .forEach((track => track.enabled = !track.enabled));
-//     if(!muted){
-//         muteBtn.innerText = "Unmute";
-//         muted =true;
-//     } else {
-//         muteBtn.innerText = "Mute";
-//         muted = false;
-//     }
-// }
-// function handleCameraClick () {
-//     myStream
-//         .getVideoTracks()
-//         .forEach((track => track.enabled = !track.enabled));
-//     if(cameraOff){
-//         cameraBtn.innerText = "Turn Camera Off";
-//         cameraOff = false;
-//     } else {
-//         cameraBtn.innerText = "Turn Camera On";
-//         cameraOff = true;
-//     }
-// }
-
-// async function handleCameraChange(){
-//     await getMedia(camerasSelect.value);
-// }
-
-// muteBtn.addEventListener("click", handleMuteClick);
-// cameraBtn.addEventListener("click", handleCameraClick);
-// camerasSelect.addEventListener("input", handleCameraChange);
 
 // //Welcome Form (join a room)
 
@@ -280,7 +194,7 @@ function handleAddstream(data){
 
 // welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 
-// //Socket Code
+// ===== Socket Code
 
 // socket.on("welcome", async () => {
 //     const offer = await myPeerConnection.createOffer();
